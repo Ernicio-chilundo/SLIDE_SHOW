b45b9bcd91618ea29e64518beba81e21e2414e37
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-01 10:34:09 +0200

    criado o projeto

---
+++ slideshow.js
@@ -0,0 +1,30 @@
+'use string'
+
+const images = [
+    { "id": "1", "url": "./img/chrono.jpg" },
+    { "id": "2", "url": "./img/inuyasha.jpg" },
+    { "id": "3", "url": "./img/ippo.npg" },
+    { "id": "2", "url": "./img/tenchi.jpg" },
+    { "id": "2", "url": "./img/tenjhotenge.jpg" },
+    { "id": "2", "url": "./img/yuyuhakusho.jpg" },
+]
+
+const containerItems = document.getElementById("container_items")
+const loadImages = (images, containerItems) => {
+    images.forEach(image => {
+        containerItems.innerHTML = `
+        <div class="item">
+            <img src="${image.url}"
+        </div>
+        `
+    })
+}
+loadImages(images, containerItems)
+let items = document.querySelectorAll(".item")
+
+const previous = () => {
+    containerItems.appendChild(items[0])
+    items = document.querySelectorAll(".item")
+}
+
+const nest = 
