3135dfd630ed0532642b2bf949ac5ab663705d3a
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-01 11:25:29 +0200

    fim

--- slideshow.js
+++ slideshow.js
@@ -1,9 +1,8 @@
-'use string'
 
 const images = [
     { "id": "1", "url": "./img/chrono.jpg" },
     { "id": "2", "url": "./img/inuyasha.jpg" },
-    { "id": "3", "url": "./img/ippo.npg" },
+    { "id": "3", "url": "./img/ippo.png" },
     { "id": "2", "url": "./img/tenchi.jpg" },
     { "id": "2", "url": "./img/tenjhotenge.jpg" },
     { "id": "2", "url": "./img/yuyuhakusho.jpg" },
@@ -12,9 +11,9 @@ const images = [
 const containerItems = document.getElementById("container_items")
 const loadImages = (images, containerItems) => {
     images.forEach(image => {
-        containerItems.innerHTML = `
+        containerItems.innerHTML += `
         <div class="item">
-            <img src="${image.url}"
+            <img src="${image.url}">
         </div>
         `
     })
@@ -27,4 +26,11 @@ const previous = () => {
     items = document.querySelectorAll(".item")
 }
 
-const nest = 
+const next = () => {
+    const lastItem = items[items.length - 1]
+    containerItems.insertBefore(lastItem, items[0])
+    items = document.querySelectorAll(".item")
+}
+
+document.getElementById("previous").addEventListener("click", previous)
+document.getElementById("next").addEventListener("click", next)
